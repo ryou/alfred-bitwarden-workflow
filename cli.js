3b071a15c2e2c799807d883081a6decdd523a61b
ryou <visualjupiter@gmail.com>
2019-09-07 13:56:14 +0900

    fix: envファイルの生成が正常に動作するように

--- cli.js
+++ cli.js
@@ -3,19 +3,36 @@ const program = require('commander')
 const fs = require('fs-extra')
 const { execAsync } = require('./util')
 
-program
-  .command('login')
-  .description('login bitwarden and get session key.')
-  .action(async () => {
-    await execAsync('npx bw login')
-  })
+const projectRootPath = __dirname
+const bwPath = `${projectRootPath}/node_modules/.bin/bw`.replace(/ /g, '\\ ')
+
+const generateEnvFile = sessionKey => {
+  const data = `BW_SESSION=${sessionKey}\n`
+  const outputPath = `${projectRootPath}/.env`
+
+  return fs.outputFile(outputPath, data)
+}
+
+const logout = () => {
+  const command = `${bwPath} logout`
+
+  return execAsync(command)
+}
+
+const login = (username, password, code) => {
+  const command = `${bwPath} login ${username} ${password} --code ${code} --raw`
+
+  return execAsync(command)
+}
 
 program
-  .command('generate <sessionKey>')
-  .description('generate env file with session key. (if env file already exist. overwrite it.)')
-  .action(async sessionKey => {
-    const data = `BW_SESSION=${sessionKey}\n`
-    fs.outputFile('.env', data)
+  .command('init <username> <password> <code>')
+  .description('login bitwarden and generate env file.')
+  .action(async (username, password, code) => {
+    await logout()
+    const sessionKey = await login(username, password, code)
+    await generateEnvFile(sessionKey)
+    console.log(`generate env file with session key ${sessionKey}`)
   })
 
 program.parse(process.argv)
