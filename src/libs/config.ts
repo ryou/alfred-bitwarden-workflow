b0efab53ecc2424525bc09837e7b4bcd2620370c
ryou <visualjupiter@gmail.com>
2019-09-10 23:14:27 +0900

    fix: グローバルインストールした際にプロジェクトルートパスが正常に取得できていない不具合の修正

--- src/libs/config.ts
+++ src/libs/config.ts
@@ -1,6 +1,18 @@
-import appRootPath from 'app-root-path'
+import { resolve } from 'path'
+import { pathExists } from 'fs-extra'
 
-export const PROJECT_ROOT_PATH = appRootPath.path
+export const PROJECT_ROOT_PATH = resolve(__dirname, '../../')
+
+// パスの設定ミスを防ぐために、PROJECT_ROOT_PATHにpackage.jsonが存在していることを確認する
+const ensureProjectRootPathIsCorrect = async () => {
+    const packageJsonExistsAtRoot = await pathExists(
+        `${PROJECT_ROOT_PATH}/package.json`
+    )
+    if (!packageJsonExistsAtRoot) {
+        throw new Error('PROJECT_ROOT_PATHの設定が間違っています。')
+    }
+}
+ensureProjectRootPathIsCorrect()
 
 export const CACHE_KEY = 'list'
 
